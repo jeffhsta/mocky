9b86a8b18e4ab5d0beded51eeb83f2ac3727a926
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-09-24 03:47:33 -0300

    Add lib descriptions and package information

--- mix.exs
+++ mix.exs
@@ -7,11 +7,27 @@ defmodule Mocky.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  def application, do:
+  def application do
     [extra_applications: [:logger], mod: {Mocky.Application, []}]
+  end
 
-  defp deps, do: []
+  defp deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev},
+     {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description, do: "Elixir mock library"
+
+  defp package do
+    [maintainers: ["Jefferson Stachelski"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/jeffhsta/mocky"}
+    ]
+  end
 end
